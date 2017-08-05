36578cc2d50e786b3eb88b052cd58365f6917f25
featheroffalcon <featheroffalcon@gmail.com>
2017-08-05 15:06:48 +0200

    Added timer

--- imge/imge.java
+++ imge/imge.java
@@ -43,36 +43,23 @@ public class imge
 				
 				for (int currentbyte = startbyte;currentbyte < fileArray.length-1;currentbyte = currentbyte + 1)
 				{
-					int currentcolor = fileArray[currentbyte];//*1000000+fileArray[currentbyte+1]*1000+fileArray[currentbyte+2];
-					int colorright   = fileArray[currentbyte+1];//*1000000+fileArray[currentbyte+4]*1000+fileArray[currentbyte+5];
-					int colordown = currentcolor;
-					// try
-					// {
-						// colordown = fileArray[currentbyte-width];
-					// }
-					// catch (Exception e)
-					// {
-						
-					// }
+					int currentcolor = fileArray[currentbyte];
+					int colorright   = fileArray[currentbyte+1];
 					
-					if(currentcolor != colorright || currentcolor != colordown)
+					if(currentcolor != colorright)
 					{
 						
-						newArray[currentbyte]     = (byte) 0b0;
-						//newArray[currentbyte + 1] = (byte) 0b0;
-						//newArray[currentbyte + 2] = (byte) 0b0;
+						newArray[currentbyte]     = (byte) 0b11111111;
 					}
 					else
 					{
-						newArray[currentbyte]     = (byte) 0b111111111;
-						//newArray[currentbyte + 1] = (byte) 0b11111111;
-						//newArray[currentbyte + 2] = (byte) 0b11111111;
-						
+						newArray[currentbyte]     = (byte) 0b0;
 					}
 					
 				}
 				System.out.println("Give the filename plus '.bmp' you want to save this in:");
 				eingabestring = eingabe.next();
+				long starttime = System.currentTimeMillis();
 				filewrite = new FileOutputStream(eingabestring);
 				
 				for (int y=0; y<newArray.length; y++)
@@ -81,6 +68,9 @@ public class imge
 				}
 				filewrite.close();
 				eingabestring = "";
+				long endtime = System.currentTimeMillis();
+				long time = endtime - starttime;
+				System.out.println("Took " + time + " milliseconds");
 			}
 			
 			else if(eingabestring.equals("end"))
